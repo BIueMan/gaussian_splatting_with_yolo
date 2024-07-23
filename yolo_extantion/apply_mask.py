70252b26a5de16dc569c442b6d462a1d03266fd4
Dan Ben David <d_bendavid@apple.com>
2024-07-23 16:45:20 +0300

    create a function out of applay mask

--- yolo_extantion/apply_mask.py
+++ yolo_extantion/apply_mask.py
@@ -2,44 +2,37 @@ import numpy as np
 import cv2
 import os
 
-# Load the .npy file containing the masks
-mask_file_path = 'output/mask_output.npy'
-masks = np.load(mask_file_path)
 
-# Directory paths
-image_dir = 'data/to_apply_mask'
-output_dir = 'output/apply_mask'
+def apply_mask(mask_file_path:str, images_dir:str):
+    ##### make sure `images_dir` have only the images in it ####
+    masks = np.load(mask_file_path)
+    output_dir = images_dir
 
-# Create output directory if it doesn't exist
-os.makedirs(output_dir, exist_ok=True)
+    for i in range(len(os.listdir(images_dir))):
+        # Read the image
+        image_number = f"{i+1:03d}.png"
+        image_path = os.path.join(images_dir, image_number)
+        image = cv2.imread(image_path)
 
-# Iterate through the images and corresponding masks
-for i in range(16):
-    # Read the image
-    image_number = f"{i+1:03d}.png"
-    image_path = os.path.join(image_dir, image_number)
-    image = cv2.imread(image_path)
+        if image is None:
+            print(f"Image {image_number} could not be read.")
+            continue
 
-    print(image_number)
-    if image is None:
-        print(f"Image {image_number} could not be read.")
-        continue
+        # Resize mask to the shape of the image
+        mask = masks[i]
+        mask_resized = cv2.resize(mask, (image.shape[1], image.shape[0]))
+        # make sure the Threshold of the mask are binary (0 or 1)
+        mask_boolean = mask_resized > 0
+        mask_float = mask_boolean.astype(np.float32)
+        # Apply the mask
+        masked_image = image * mask_float[:, :, np.newaxis]
+        # Save the masked image
+        output_path = os.path.join(output_dir, f"{image_number}")
+        cv2.imwrite(output_path, masked_image)
 
-    # Get the corresponding mask
-    mask = masks[i]
+    print(f"Processed and saved: {output_dir}")
 
-    # Resize the mask to the shape of the image
-    mask_resized = cv2.resize(mask, (image.shape[1], image.shape[0]))
-
-    # Threshold the mask to binary (0 or 1)
-    mask_boolean = mask_resized > 0
-    mask_float = mask_boolean.astype(np.float32)
-
-    # Apply the mask to the image
-    masked_image = image * mask_float[:, :, np.newaxis]
-
-    # Save the masked image
-    output_path = os.path.join(output_dir, f"{image_number}")
-    cv2.imwrite(output_path, masked_image)
-
-    print(f"Processed and saved: {output_path}")
+if __name__ == "__main__":
+    mask_file_path = 'output/mask_output.npy'
+    images_dir = 'data/to_apply_mask'
+    apply_mask(mask_file_path, images_dir)
